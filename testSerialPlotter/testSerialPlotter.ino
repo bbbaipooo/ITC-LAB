a9ef13587107f310daeb5332be3ac8c8817b9d64
BAIPOO <baipoo_B@hotmail.com>
2023-09-08 03:01:05 +0700

    add

---
+++ testSerialPlotter/testSerialPlotter.ino
@@ -0,0 +1,11 @@
+int sensorpin=A0;
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorValue=analogRead(A0);
+  float voltage=sensorValue*(5.0/1023.0);
+  Serial.println(voltage);
+}
